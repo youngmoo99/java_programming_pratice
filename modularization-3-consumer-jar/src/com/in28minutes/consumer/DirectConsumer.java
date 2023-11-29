1e778a24a0b8de1012db61816e1e32861ae92e17
YoungMoo Kim <90688872+youngmoo99@users.noreply.github.com>
2023-11-29 14:43:27 +0900

    Add files via upload

---
+++ modularization-3-consumer-jar/src/com/in28minutes/consumer/DirectConsumer.java
@@ -0,0 +1,19 @@
+package com.in28minutes.consumer;
+
+import java.util.List;
+import java.util.logging.Logger;
+
+import com.in28minutes.sorting.algorithm.BubbleSort;
+
+public class DirectConsumer {
+	// 로깅 모듈
+	private static Logger logger = Logger.getLogger(DirectConsumer.class.getName());
+
+	public static void main(String[] args) {
+		BubbleSort util = new BubbleSort();
+
+		List<String> sorted = util.sort(List.of("Ranga", "Ravi", "Sathish", "Adam", "Eve"));
+		logger.info(sorted.toString());
+	}
+
+}
