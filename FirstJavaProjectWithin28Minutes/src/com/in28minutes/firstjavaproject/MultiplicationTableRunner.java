c838411931fe4748dd8f57fcaea5cfd8979b3bc8
YoungMoo Kim <90688872+youngmoo99@users.noreply.github.com>
2023-11-29 14:39:36 +0900

    Add files via upload

---
+++ FirstJavaProjectWithin28Minutes/src/com/in28minutes/firstjavaproject/MultiplicationTableRunner.java
@@ -0,0 +1,11 @@
+package com.in28minutes.firstjavaproject;
+
+public class MultiplicationTableRunner {
+
+	public static void main(String[] args) {
+		MultiplicationTable table = new MultiplicationTable();
+		// table.print(12);
+		table.print();
+	}
+
+}
